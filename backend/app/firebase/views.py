dea7b96305bc6cdd5af5d4c9c0dc10c8dd3f65f2
Esteban Monguí <esteband.itorres@gmail.com>
2020-08-06 15:40:47 -0500

    manage user already exists error in signup view

--- backend/app/firebase/views.py
+++ backend/app/firebase/views.py
@@ -2,24 +2,35 @@
 # #Blueprints
 from . import auth
 #flask
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 
 #Models
 from .models import User
 
+#firebase exceptions
+from firebase_admin.auth import EmailAlreadyExistsError
+
 bp = auth.bp
 
-@bp.route('signup/', methods=('POST', 'GET'))
+@bp.route('signup/', methods=('POST'))
 def signup():
     """Logic for input and send data for create new user"""
     if request.method == 'POST':
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
-        # passwd_confirm = request.POST['password_confirmation']
-    
-        user = User(username, email, password)
-        user.create_user()
-        return render_template('signup.html')
+        passwd_confirm = request.POST['password_confirmation']
+        try:
+            user = User(username, email, password)
+            user.create_user()
+            return redirect(url_for('login'))
+        except EmailAlreadyExistsError:
+            error = 'El usuario ya está registrado, por favor verifique sus datos'
+            return render_template('signup.html', error=error)
 
     return render_template('signup.html')
+
+
+@auth.route('login', methods=('POST'))
+def login():
+    pass
