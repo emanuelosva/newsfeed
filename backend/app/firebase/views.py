d27f11a9a0081c207f417481d8e5b5440db9de65
Esteban Monguí <esteband.itorres@gmail.com>
2020-08-06 15:02:07 -0500

    integrate e user model to signup view

--- backend/app/firebase/views.py
+++ backend/app/firebase/views.py
@@ -4,15 +4,22 @@ from . import auth
 #flask
 from flask import render_template, request
 
+#Models
+from .models import User
+
 bp = auth.bp
 
 @bp.route('signup/', methods=('POST', 'GET'))
 def signup():
     """Logic for input and send data for create new user"""
     if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        passwd_confirm = request.POST['password_confirmation']
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        # passwd_confirm = request.POST['password_confirmation']
     
+        user = User(username, email, password)
+        user.create_user()
+        return render_template('signup.html')
+
     return render_template('signup.html')
