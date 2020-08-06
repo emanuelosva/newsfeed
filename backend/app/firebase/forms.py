bf0b85ecc1bdfff29e42d04d162bd226310f673d
Esteban Monguí <esteband.itorres@gmail.com>
2020-08-06 15:49:13 -0500

    create logic for validation passwords of signup form

---
+++ backend/app/firebase/forms.py
@@ -0,0 +1,17 @@
+"""Logic of background of forms"""
+
+class SignupForms():
+    def __init__(self, username, email, password, passwd_confirm):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.passwd_conf = passwd_confirm
+
+    def signup_passwords(self):
+        """Comparate password and password confirmation from signup form,
+        return boolean data type.
+        """
+        if self.password == self.passwd_confirm:
+            return True
+        else:
+            return False 
