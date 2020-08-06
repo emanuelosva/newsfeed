5f3ce559a9af5b0df5a0d129d85bdac4acf3a188
Esteban Monguí <esteband.itorres@gmail.com>
2020-08-06 10:34:58 -0500

    add description to signup view

--- backend/app/firebase/views.py
+++ backend/app/firebase/views.py
@@ -6,10 +6,11 @@ from flask import render_template
 
 @fb_auth('/signup', methods=('POST'))
 def signup():
+    """Logic for input and send data for create new user"""
     if request.method == 'POST':
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
         passwd_confirm = request.POST['password_confirmation']
     
-    return render_template('signup.html')
+    return render_template('signup.html', name=signup)
