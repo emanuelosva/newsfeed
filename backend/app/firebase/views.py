92af249f133750410c5936f5d97fba1ffa48ba63
Esteban Monguí <esteband.itorres@gmail.com>
2020-08-06 20:03:25 -0500

    create login view logic

--- backend/app/firebase/views.py
+++ backend/app/firebase/views.py
@@ -10,9 +10,9 @@ from firebase_admin.auth import EmailAlreadyExistsError
 #Forms
 from .forms import SignupForms
 #password hash
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 #Firebase services
-from .firestore_service import user_add, get_user
+from .firestore_service import user_add, get_user_by_email
 
 bp = auth.bp
 
@@ -24,18 +24,34 @@ def signup():
         email = request.form['email']
         password = request.form['password']
 
-        user_validate = get_user(username)
+        user_validate = get_user_by_email(email)
         if user_validate.to_dict() is None:
             password_hash = generate_password_hash(password)
             user_data = UserData(username, password_hash, email)
             user_add(user_data)
             return redirect(url_for('auth.login'))
         else:
-            flash('El usuario ya existe. \n Por favor valide la información')
+            error = 'El usuario ya existe, por favor valide la información'
+            return render_template('signup.html', error=error)
     
     return render_template('signup.html')
 
 
 @bp.route('login', methods=['POST', 'GET'])
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user_validate = get_user_by_email(email)
+        if user_validate is not None:
+            password_from_db = user_validate.to_dict()['password']
+            check_password = check_password_hash(password_from_db, password)
+            if check_password:
+                return redirect(url_for('auth.feed'))
+            else:
+                flash('Contraseña o nombre de usuario incorrectos')
+        else:
+            flash('El usuario no existe, verifique su correo')
+
     return render_template('login.html')
