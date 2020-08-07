98e7a1751ce9d42de4a0606c9da5af4e0fd0a0f8
Esteban Monguí <esteband.itorres@gmail.com>
2020-08-07 13:07:56 -0500

    create add and delete news site logic in firevbase service

--- backend/app/firebase/firestore_service.py
+++ backend/app/firebase/firestore_service.py
@@ -16,6 +16,7 @@ def user_add(user_data):
     user.set({
         'username':user_data.username,
         'password':user_data.password,
+        'news_sites': []
     })
 
 
@@ -25,5 +26,13 @@ def get_user_by_email(user_email):
 
 
 def add_news_site(user_id, news_site_name):
-    news_collection = db.collection('users').document(user_id).collection('news_sites')
-    news_collection.add({'news_site_name':news_site_name})
+    news_sites = db.collection('users').document(user_id).news_sites
+    news_sites.add(news_site_name)
+
+
+def delete_news_site(user_id, news_site_name):
+    news_sites = db.collection('users').document(user_id).news_sites
+    
+    for news_site in news_sites:
+        if news_site == news_site_name:
+            news_site.delete()
