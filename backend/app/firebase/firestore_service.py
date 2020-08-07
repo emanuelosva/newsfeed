3cd68d215a2ccae1f94b07d9e98ab6831944c9e2
Esteban Monguí <esteband.itorres@gmail.com>
2020-08-06 19:39:35 -0500

    change get_user method by get_user_email method

--- backend/app/firebase/firestore_service.py
+++ backend/app/firebase/firestore_service.py
@@ -12,10 +12,13 @@ db = firestore.client()
 
 def user_add(user_data):
     """Add new user to database"""
-    user = db.collection('users').document(user_data.username)
-    user.set({'password':user_data.password})
+    user = db.collection('users').document(user_data.email)
+    user.set({
+        'username':user_data.username,
+        'password':user_data.password,
+    })
 
 
-def get_user(user_id):
+def get_user_by_email(user_email):
     """get user by UID from database"""
-    return db.collection('users').document(user_id).get()
+    return db.collection('users').document(user_email).get()
