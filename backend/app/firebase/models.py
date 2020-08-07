3c8037b7b180cb5e6ae4928dd73b619fdd7d6030
Esteban Monguí <esteband.itorres@gmail.com>
2020-08-06 19:00:46 -0500

    restart models file

--- backend/app/firebase/models.py
+++ backend/app/firebase/models.py
@@ -1,27 +1,4 @@
-"""File of users models"""
-#Firebase services
-import firebase_admin
-from firebase_admin import auth
-from google.cloud import storage
-
-storage_client = storage.Client()
-default_app = firebase_admin.initialize_app()
-
-class User():
-    """Logic for create and authenticate users"""
-
-    def __init__(self, username, email, password):
-        self.username = username
-        self.email = email
-        self.password = password
-
-    def create_user(self):
-        firebase_admin.auth.create_user(
-            display_name = self.username,
-            email = self.email, 
-            password= self.password,
-            email_verified = False 
-        )
+"""File of models"""
 
 
 class UserData():
