d382bd7d5f51090d327ce537759e8f350084cb04
Esteban Monguí <esteband.itorres@gmail.com>
2020-08-07 12:11:06 -0500

    Create add news logic in feed views

--- backend/app/firebase/feed/views.py
+++ backend/app/firebase/feed/views.py
@@ -1,20 +1,31 @@
 """News feed view"""
 
-#flask
-from flask import render_template, redirect, request, session, url_for
-from flask_login import login_required, current_user
 #blueprint
 from . import bp
+#flask
+from flask import render_template, redirect, request, session, url_for, flash
+from flask_login import login_required, current_user
+#firestore service
+from app.firebase.firestore_service import add_news_site
 
 
 @bp.route('feed/', methods=['POST', 'GET'])
 @login_required
 def feed():
+    email = current_user.id
+    if request.method == 'POST':
+        if request.form == request.form['add_new']:
+            return redirect(url_for('feed'))
+
     return render_template('feed.html')
 
-@bp.route('', methods=['POST'])
-def add_news():
-    user_id = current_user.username
 
+@bp.route('feed/add_news', methods=['POST'])
+def add_news():
+    if request.method == 'POST':
+        email = current_user.id
+        news_site_name = request.form['news_site_name']
+        add_news_site(email, news_site_name)
+        flash(f'Se agregado {news_site_name} a su lista de favoritos')
     return redirect(url_for('feed'))
     
