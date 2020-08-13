c7892efc5076087f83fa72278c8106032cec681a
emanuelosva <emanuelosva@gmail.com>
2020-08-13 12:50:23 -0500

    Custom Store for manage user data

---
+++ web/src/store/user.js
@@ -0,0 +1,28 @@
+import { writable } from 'svelte/store';
+
+const USER = {
+  username: null,
+  news_sites: [],
+};
+
+function getUser() {
+  const { subscribe, update, set } = writable(USER)
+
+  return {
+    subscribe,
+    set: data => set({ ...data }),
+    update: data => update(user => ({ ...user, ...data })),
+    addNews: newsName => update(user => {
+      if (user.news_sites.includes(newsName)) return user;
+      else return user.news_sites.unshift(newsName);
+    }),
+    removeNews: newsName => user.update(user => {
+      if (!user.news_sites.includes(newsName)) return user
+      const index = user.news_sites.indexOf(newsName);
+      return user.news_sites.slice(0, index);
+    }),
+    reset: () => set(USER),
+  };
+};
+
+export const user = getUser();
