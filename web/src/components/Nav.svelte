3e7bb5b448de892b9929f1b6a328cd67f2f9ce18
emanuelosva <emanuelosva@gmail.com>
2020-08-13 12:51:21 -0500

    Add logout and name after login

--- web/src/components/Nav.svelte
+++ web/src/components/Nav.svelte
@@ -1,5 +1,13 @@
 <script>
-  let name = "Dummy";
+  import { goto } from "@sapper/app";
+  import { user } from "../store/user";
+  import { serverRequest } from "../utils/apiRequest";
+
+  const logout = async () => {
+    const { data } = await serverRequest("/server/logout", "POST");
+    user.reset();
+    await goto("/");
+  };
 </script>
 
 <header class="container mx-auto">
@@ -9,16 +17,30 @@
     </h1>
     <ul class="flex">
       <li>
-        <a class="font-thin text-gray-500 mr-4" href="/signup">Register</a>
+        {#if $user.username}
+          <p class="font-thin text-gray-500 mr-4">{$user.username}</p>
+        {:else}
+          <a class="font-thin text-gray-500 mr-4" href="/signup">Register</a>
+        {/if}
       </li>
       <li>
-        <a
-          class="font-semibold text-orange-500 uppercase border
-          border-orange-500 px-8 py-2 rounded-md hover:bg-orange-500
-          hover:text-orange-100"
-          href="/login">
-          Login
-        </a>
+        {#if $user.username}
+          <button
+            on:click={logout}
+            class="font-semibold text-orange-500 uppercase border
+            border-orange-500 px-8 py-2 rounded-md hover:bg-orange-500
+            hover:text-orange-100">
+            Logout
+          </button>
+        {:else}
+          <a
+            class="font-semibold text-orange-500 uppercase border
+            border-orange-500 px-8 py-2 rounded-md hover:bg-orange-500
+            hover:text-orange-100"
+            href="/login">
+            Login
+          </a>
+        {/if}
       </li>
     </ul>
   </nav>
