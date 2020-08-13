10f5f40228b5e8f91386f13db07a5baf26a9bed4
emanuelosva <emanuelosva@gmail.com>
2020-08-13 12:49:45 -0500

    Proxy endponits and security & session manage

--- web/src/server.js
+++ web/src/server.js
@@ -1,17 +1,66 @@
 import sirv from 'sirv';
-import polka from 'polka';
 import compression from 'compression';
+import helmet from 'helmet';
 import * as sapper from '@sapper/server';
+import axios from 'axios';
+const coockieSession = require('cookie-session');
+const express = require('express');
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV !== 'production';
+const API = 'https://newsfeedapi.vercel.app';
 
-polka() // You can also use Express
-  .use(
-    compression({ threshold: 0 }),
-    sirv('static', { dev }),
-    sapper.middleware()
-  )
-  .listen(PORT, err => {
-    if (err) console.log('error', err);
-  });
+const server = express();
+
+// Security headers
+server.use(helmet());
+
+// Parse req
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+// Session manage
+server.use(coockieSession({
+  secure: !dev,
+  httpOnly: !dev,
+  secret: process.env.SECRET_COCKIE || 'secret',
+  maxAge: 1000 * 60 * 60 * 24 * 5,
+}));
+
+
+/**
+ * ------------- Proxy api routes --------------
+ */
+
+// Manage login
+server.post('/server/login', async (req, res) => {
+  const { body } = req;
+  try {
+    const { status, data } = await axios({
+      url: `${API}/users/login`,
+      method: 'POST',
+      data: { ...body },
+    });
+
+    req.session.token = data.data.token;
+    res.send(data.data.user);
+  } catch (error) {
+    res.status(error.response.status).send(error)
+  }
+});
+
+// Destroy coockie session
+server.post('/server/logout', (req, res) => {
+  req.session = null;
+  res.status(200).send('Logout')
+});
+
+// Static server
+server.use(compression({ threshold: 0 }));
+server.use(sirv('static', { dev }));
+server.use(sapper.middleware());
+
+// Server initialization
+server.listen(PORT, (err) => {
+  if (err) throw new Error('Server Error');
+});
